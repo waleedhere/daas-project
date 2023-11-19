f025342f6de42741b4f67e010f7dd42271959262
waleedhere <52565365+waleedhere@users.noreply.github.com>
2023-11-19 15:47:30 +0100

    Update remote-state.tf

--- terraformTemplates/azure/acr/remote-state.tf
+++ terraformTemplates/azure/acr/remote-state.tf
@@ -7,6 +7,6 @@ terraform {
     use_msi              = true
     subscription_id      = "a6f413aa-1a8e-41a8-b55b-9e20f8caffec"
     tenant_id            = "085121fe-7e3c-46d8-b86b-25ab5d024d63"
-    access_key           = "VetmUH8UkU+gkmattU4XwQbDmkw/jAWrZYALx87CAVQBsyntwYtXW5KbrmsBlDjOolx5QgavMB7e+AStTEPRHQ=="
+    access_key           = "xxxxx"
   }
 }
